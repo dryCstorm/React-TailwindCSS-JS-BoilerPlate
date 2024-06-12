7280fae0dcf4d0ec08ec7a7012d1dd12e1c9a57b
Dry Storm <dekov981@outlook.com>
2024-06-12 16:02:34 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import { HomePage } from "./pages";
+
+function App() {
+  return <HomePage></HomePage>
+}
+
+export default App;
---
+++ src/pages/home/index.js
@@ -0,0 +1,3 @@
+export const HomePage = () => {
+    return <div class="text-blue-600">Hello World!</div>
+}
